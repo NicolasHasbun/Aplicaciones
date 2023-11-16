e60f376c9247370c44c21f02c47b4f2af1ddb736
NicolasHasbun <Ni.Hasbun.Molina@gmail.com>
2023-11-15 21:48:42 -0300

    cambio de texto

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nulla arcu, volutpat.</Text>
+      <Text>Hola, Coder!</Text>
       <StatusBar style="auto" />
     </View>
   );
